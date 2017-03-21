43b45269a420181e4b804971ca965b7bcb44dd1d
LeoCHANGcn <zhangshichaochina@gmail.com>
2017-03-21 19:53:36 +1100

    Person

---
+++ Assignment1/src/Person.java
@@ -0,0 +1,16 @@
+
+public abstract class Person {
+	private int ID;
+	private String name;
+	private int age;
+	private String state;
+	
+	public Person(int id,String name,int age, String state)
+	{
+		this.ID=id;
+		this.name=name;
+		this.age=age;
+		this.state=state;
+	}
+
+}
